9c812dfa1407b990bc2d5086d577b1d1ad661953
chellesjord <m.kayejordan@gmail.com>
2022-03-12 00:05:50 -0600

    initial commit. Added modules and package-json

---
+++ models/Thought.js
@@ -0,0 +1,60 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        validate: [({ length }) => length <= 280, 'You have exceeded the maximum amount of characters, 280.']
+    },
+    username: {
+        type: String,
+        required: 'Username is required',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+},
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'Text entry is required',
+            validate: [({ length }) => length <= 280, 'You have exceeded the maximum amount of characters, 280.']
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        //the user that created this thought
+        username: {
+            type: String,
+            required: 'Username is required'
+        },
+        //like replies
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'Username is required',
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: 'Email is required',
+            unique: true,
+            match: [/.+@.+\..+/]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        // prevents virtuals from creating duplicate of _id as `id`
+        id: false
+    }
+);
+
+// get total count of friends the users has int the friends array on query
+FriendSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+
+const User = model('User', UserSchema);
+
+module.exports = User;
