0c41d0f48f4d9279130ab6b0bb0ff8ea15d9b315
chellesjord <m.kayejordan@gmail.com>
2022-03-13 20:55:57 -0500

    branch merge

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -1,36 +1,36 @@
 const { Thought, User } = require('../models');
 
 const thoughtController = {
-  // get all thoughts
-  getAllThoughts(req, res) {
-    Thought.find({})
-      .populate({
-        path: 'reactions',
-        select: '-__v'
-      })
-      .select('-__v')
-      .sort({ _id: -1 })
-      .then(dbThoughtData => res.json(dbThoughtData))
-      .catch(err => {
-        console.log(err);
-        res.sendStatus(400);
-      });
-  },
+  // // get all thoughts
+  // getAllThoughts(req, res) {
+  //   Thought.find({})
+  //     .populate({
+  //       path: 'reactions',
+  //       select: '-__v'
+  //     })
+  //     .select('-__v')
+  //     .sort({ _id: -1 })
+  //     .then(dbThoughtData => res.json(dbThoughtData))
+  //     .catch(err => {
+  //       console.log(err);
+  //       res.sendStatus(400);
+  //     });
+  // },
 
-  // get one thought by id
-  getThoughtById({ params }, res) {
-    Thought.findOne({ _id: params.id })
-      .populate({
-        path: 'reactions',
-        select: '-__v'
-      })
-      .select('-__v')
-      .then(dbThoughtData => res.json(dbThoughtData))
-      .catch(err => {
-        console.log(err);
-        res.sendStatus(400);
-      });
-  },
+  // // get one thought by id
+  // getThoughtById({ params }, res) {
+  //   Thought.findOne({ _id: params.id })
+  //     .populate({
+  //       path: 'reactions',
+  //       select: '-__v'
+  //     })
+  //     .select('-__v')
+  //     .then(dbThoughtData => res.json(dbThoughtData))
+  //     .catch(err => {
+  //       console.log(err);
+  //       res.sendStatus(400);
+  //     });
+  // },
 
   // add thought to user
   addThought({ params, body }, res) {
